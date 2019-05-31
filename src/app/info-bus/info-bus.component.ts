78c5f991b8f26189d72e8cbc1f59757dce5aef39
Daniele Rosada <danielerosada98@gmail.com>
2019-05-31 17:45:15 +0200

    socket.io

--- src/app/info-bus/info-bus.component.ts
+++ src/app/info-bus/info-bus.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StorageService } from '../storage.service';
 import * as moment from 'moment';
+import * as io from 'socket.io-client';
+
 @Component({
   selector: 'app-info-bus',
   templateUrl: './info-bus.component.html',
@@ -8,7 +10,10 @@ import * as moment from 'moment';
 })
 export class InfoBusComponent implements OnInit {
   @Input() buses = [];
-  constructor(private storage: StorageService) { }
+  socket: SocketIOClient.Socket;
+  constructor(private storage: StorageService) {
+    this.socket = io.connect('http://127.0.0.1:5432');
+  }
 
   ngOnInit() {
     this.loadBusesMenu();
@@ -24,6 +29,9 @@ export class InfoBusComponent implements OnInit {
       this.storage.selectedBus(bus.busCode).then(res => {
         res.forEach(b => {
           bus.positions.push(b);
+        });
+        this.socket.on(bus.busCode, (msg: any) => {
+            console.log(msg);
         });
       });
     }
